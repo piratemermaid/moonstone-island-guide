fd6e7dc662dd94634e57dc22caae543139871923
Ariel <thepiratemermaid4@gmail.com>
2024-04-20 17:16:17 -0600

    remove template boilerplate files

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,5 +1,3 @@
-import PageOne from '../pages/PageOne';
-import PageTwo from '../pages/PageTwo';
 import CropsPage from '../pages/CropsPage';
 
 const ROUTES = [
@@ -7,16 +5,6 @@ const ROUTES = [
     label: 'Crops',
     path: '/',
     Component: CropsPage,
-  },
-  {
-    label: 'Page One',
-    path: '/one',
-    Component: PageOne,
-  },
-  {
-    label: 'Page Two',
-    path: '/two',
-    Component: PageTwo,
   },
 ];
 
