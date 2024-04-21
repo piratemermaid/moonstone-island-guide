b5719d3302b900dea1947a5c14f5351941206038
Ariel <thepiratemermaid4@gmail.com>
2024-04-20 18:20:41 -0600

    move best crop logic into component

--- src/components/crops/BestCropUI.jsx
+++ src/components/crops/BestCropUI.jsx
@@ -1,26 +1,59 @@
-import { Box, Card, CardBody, Center, Text } from '@chakra-ui/react';
+import { Card, CardBody, Center, Text } from '@chakra-ui/react';
+
+import CROPS from '../../data/crops';
 import { useSeasonColor } from '../../styles/useStyleUtils';
 
-export default function BestCropUI({ label, crop }) {
+export default function BestCropUI({ label, stat, isPositive = true }) {
+  const getBestCrop = () => {
+    let bestStat = 0;
+    let bestCrop = null;
+
+    for (let crop of CROPS) {
+      const statToCompare = crop.stats[stat];
+
+      if (statToCompare) {
+        let isCropBest = false;
+
+        if (isPositive) {
+          if (statToCompare > bestStat) isCropBest = true;
+        } else {
+          if (statToCompare < bestStat) isCropBest = true;
+        }
+
+        if (isCropBest) {
+          bestStat = statToCompare;
+          bestCrop = { ...crop, winningStat: statToCompare };
+        }
+      }
+    }
+
+    return bestCrop;
+  };
+
+  const crop = getBestCrop();
+  const { name, season, winningStat } = crop;
+
   return (
     <Card>
       <CardBody>
         <Text fontSize="lg" color="blue.300">
           {label}
         </Text>
-        <Text>{crop.name}</Text>
+        <Text>{name}</Text>
+        <Text color="green.200">{winningStat}</Text>
         <Center>
           <img
-            src={`/images/crops/${crop.name.replaceAll(' ', '')}.${
-              crop.name === 'Spectre Flower' ? 'png' : 'webp'
+            src={`/images/crops/${name.replaceAll(' ', '')}.${
+              name === 'Spectre Flower' ? 'png' : 'webp'
             }`}
-            alt={crop.name}
-            title={crop.name}
+            alt={name}
+            title={name}
             style={{ width: 34, height: 34 }}
           />
         </Center>
-        <Text sx={{ color: useSeasonColor(crop.season) }}>{crop.season}</Text>
-        <Text></Text>
+        <Text sx={{ color: useSeasonColor(season) }}>{season}</Text>
+        <Text>Days: {crop.daysToGrow}</Text>
+        <Text>Regrows: {crop.reharvest ? 'Y' : 'N'}</Text>
       </CardBody>
     </Card>
   );
--- src/components/crops/BestCropsSection.jsx
+++ src/components/crops/BestCropsSection.jsx
@@ -1,60 +1,33 @@
 import { SimpleGrid } from '@chakra-ui/react';
 
-import CROPS from '../../data/crops';
 import BestCropUI from './BestCropUI';
 import SectionHeader from './SectionHeader';
 
 export default function BestCropsSection() {
-  const getBestCrop = (stat, isPositive = true) => {
-    let bestStat = 0;
-    let bestCrop = null;
-
-    for (let crop of CROPS) {
-      const statToCompare = crop.stats[stat];
-
-      if (statToCompare) {
-        let isCropBest = false;
-
-        if (isPositive) {
-          if (statToCompare > bestStat) isCropBest = true;
-        } else {
-          if (statToCompare < bestStat) isCropBest = true;
-        }
-
-        if (isCropBest) {
-          bestStat = statToCompare;
-          bestCrop = crop;
-        }
-      }
-    }
-
-    return bestCrop;
-  };
-
   return (
     <>
       <SectionHeader>Main</SectionHeader>
       <SimpleGrid columns={4} spacing={2} sx={{ width: '100%' }}>
-        <BestCropUI label="Stamina" crop={getBestCrop('stamina')} />
-        <BestCropUI label="HP" crop={getBestCrop('hp')} />
-        <BestCropUI label="Tame" crop={getBestCrop('tame')} />
-        <BestCropUI label="EXP" crop={getBestCrop('exp')} />
+        <BestCropUI label="Stamina" stat="stamina" />
+        <BestCropUI label="HP" stat="hp" />
+        <BestCropUI label="Tame" stat="tame" />
+        <BestCropUI label="EXP" stat="exp" />
       </SimpleGrid>
 
       <SectionHeader>Team Stats</SectionHeader>
       <SimpleGrid columns={4} spacing={2} sx={{ width: '100%' }}>
-        <BestCropUI label="Armor" crop={getBestCrop('armor')} />
-        <BestCropUI label="Speed" crop={getBestCrop('speed')} />
-        <BestCropUI label="Vitality" crop={getBestCrop('vitality')} />
-        <BestCropUI label="Power" crop={getBestCrop('power')} />
+        <BestCropUI label="Armor" stat="armor" />
+        <BestCropUI label="Speed" stat="speed" />
+        <BestCropUI label="Vitality" stat="vitality" />
+        <BestCropUI label="Power" stat="power" />
       </SimpleGrid>
 
       <SectionHeader>Enemy Stats</SectionHeader>
       <SimpleGrid columns={4} spacing={2} sx={{ width: '100%' }}>
-        <BestCropUI label="Armor" crop={getBestCrop('armor', false)} />
-        <BestCropUI label="Speed" crop={getBestCrop('speed', false)} />
-        <BestCropUI label="Vitality" crop={getBestCrop('vitality', false)} />
-        <BestCropUI label="Power" crop={getBestCrop('power', false)} />
+        <BestCropUI label="Armor" stat="armor" isPositive={false} />
+        <BestCropUI label="Speed" stat="speed" isPositive={false} />
+        <BestCropUI label="Vitality" stat="vitality" isPositive={false} />
+        <BestCropUI label="Power" stat="power" isPositive={false} />
       </SimpleGrid>
     </>
   );
